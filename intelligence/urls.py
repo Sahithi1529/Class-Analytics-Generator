bf30ad4efe4b187778840ac2393f1c3605406456
KARTHIK RAO <karthikraopadala@gmail.com>
2025-02-07 22:07:49 +0530

    Update Bar graph facility

--- intelligence/urls.py
+++ intelligence/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('test',views.Test),
+    # path('test',views.Test),
     path('start-tracking',views.startTracking),
-    path('generate-analytics',views.generateAnalytics),
+    path('generate-analytics',views.generateAnalytics,name="generate-analytics"),
     path('show-analytics',views.showAnalytics,name="show-analytics")
    
 ]
--- intelligence/views.py
+++ intelligence/views.py
@@ -10,6 +10,7 @@ from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import io
+from django.views.decorators import gzip
 correct = {
     'message':"",
     'description':"",
@@ -25,73 +26,8 @@ wrong = {
     "iconCode":"#10060"
 }
 
-# GET @/intelligence/start-tracking  ---> Starts recording stats
-def startTracking(request):
-     classId = request.GET.get('classId')
-     facultyId = request.GET.get('facultyId')
-     dateAndTime = request.GET.get('dateAndTime')
-     filename = classId+"-"+facultyId+"-"+dateAndTime+".csv"
-     takePictureForEvery = 5
-     totalDuration = 1*60
-     totalFrames = totalDuration//takePictureForEvery
-     cam = dr.Camera()
-     model = dr.Model()
-     for i in range(totalFrames):
-          frame = cam.getFrame()
-          resized_frame = cam.resizeFrame(frame,(48,48))
-        #   preprocessed_frame = model.preprocess(resized_frame)
-          preprocessed_frame = resized_frame
-          predictions = model.predict(preprocessed_frame)
-          predictions['class_Id'] = [classId]
-          predictions['facultyId'] = [facultyId]
-          curr = str(datetime.today()).split()
-          predictions['timeStamp'] = [curr[1][:8]]
-          dfe = pd.DataFrame(predictions)
-          if i==0:
-               dfe.to_csv(os.path.join(settings.BASE_DIR,'static','resources',filename),index=False)
-          else:
-               dfe.to_csv(os.path.join(settings.BASE_DIR,'static','resources',filename),index=False,mode='a',header=False)
-          print(f"Analytics {i} Recorded")
-          time.sleep(takePictureForEvery)
-     correct['title'] = "Class Completed"
-     correct['message'] = "Class Analytics Generated Successfully!!"
-     return render(request,'messenger.html',correct)
-
-# GET @/intelligence/generate-analytics   ---> Generate Analytics from CSV File
-def generateAnalytics(request):
-     filename = request.GET.get('filename')
-     dfe = pd.read_csv(os.path.join(settings.BASE_DIR,'static','resources',filename))
-     timestamps = dfe.iloc[:]['timeStamp']
-     values = {
-          'Engaged':dfe.iloc[:]['Engaged'],
-          'Not Engaged':dfe.iloc[:]['Not Engaged'],
-          'Total':dfe.iloc[:]['total']
-     }
-     x = np.arange(len(timestamps))  # the label locations
-     width = 0.25  # the width of the bars
-     multiplier = 0
-     fig, ax = plt.subplots(layout='constrained')
-     ref = {
-    'Engaged':"green",
-    "Not Engaged":"red",
-    "Total":"blue"
-     }
-     for attribute, measurement in values.items():
-        offset = width * multiplier
-        rects = ax.bar(x + offset, measurement, width, label=attribute,color=ref[attribute])
-        ax.bar_label(rects, padding=3)
-        multiplier += 1
-     ax.set_ylabel('Count of Students')
-     ax.set_title('Time Stamps')
-     ax.set_xticks(x + width, timestamps)
-     ax.legend(loc='upper left', ncols=3)
-     ax.set_ylim(0, 80)
-     plt.savefig(os.path.join(settings.BASE_DIR,'static','resources','analytics.png'))
-     return render(request,'showAnalytics.html',{'filename':filename})
-
-
-def getGraph(filename):
-     
+def getGraph(request,filename):
+   for i in range(12):
      dfe = pd.read_csv(os.path.join(settings.BASE_DIR,'static','resources',filename))
      timestamps = dfe.iloc[:]['timeStamp']
      values = {
@@ -123,13 +59,20 @@ def getGraph(filename):
      buf = io.BytesIO()
      plt.savefig(buf, format="jpg")
      frame = buf.getvalue()
+     time.sleep(5)
+     if i==11:
+         correct['title'] = "Success"
+         correct['Message'] = "Class Analytics generated successfully"
+         return render(request,'messenger.html',correct)
      yield (b'--frame\r\n'
             b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-
+@gzip.gzip_page
 def showAnalytics(request):
      filename = request.GET.get('filename')
-     graph = getGraph(filename)
+     time.sleep(10)
+     graph = getGraph(request,filename)
+     print("Hello")
      return StreamingHttpResponse(graph,content_type="multipart/x-mixed-replace;boundary=frame")
 
 
@@ -148,41 +91,42 @@ def showAnalytics(request):
 
 
 
-def Test(request):
-    class_Id = request.GET.get('classId')
-    faculty_Id = request.GET.get('facultyId')
-    dateAndTime = request.GET.get('dateTime')
-    # cam = dr.Camera()
-    # model = dr.Model()
-    # frame = cam.getFrame() # We will have a gray scale image
-    # resized_frame = cam.resizeFrame(frame,(48,48))
-    # preprocessed_frame = model.preprocess(resized_frame)
-    # prediction = model.predict(preprocessed_frame) # Let us put it as Dictionary
-    for i in range(5):
-        prediction = {
-            'Bored':[2],
-            'Confused':[3],
-            'Focused':[4],
-            'Sleepy':[1],
-            'Engaged':[7],
-            'Not Engaged':[3],
-            'total': [10]
-        }
-        prediction['class_Id'] = [class_Id]
-        prediction['facultyId'] = [faculty_Id]
-        curr = str(datetime.today()).split()
-        prediction['timeStamp'] = [curr[1][:8]]
-        filename = class_Id+"-"+faculty_Id+"-"+dateAndTime+'.csv'
-        if filename in os.listdir(os.path.join(settings.BASE_DIR,'static','resources')):
-             dfe = pd.DataFrame(prediction)
-             dfe.to_csv(os.path.join(settings.BASE_DIR,'static','resources',filename),mode='a', index=False, header=False)
-        else:
-            dfe = pd.DataFrame(prediction)
-            dfe.to_csv(os.path.join(settings.BASE_DIR,'static','resources',filename),index=False)
-        print("DONE")
-        time.sleep(5)
-    return HttpResponse("Hello")
+def startTracking(request):
+     classId = request.GET.get('classId')
+     facultyId = request.GET.get('facultyId')
+     dateAndTime = request.GET.get('dateAndTime')
+     filename = classId+"-"+facultyId+"-"+dateAndTime+'.csv'
+     return render(request,'liveAnalytics.html',{'filename':filename})
 
+
+def generateAnalytics(request):
+     filename = request.GET.get('filename')
+     classId,facultyId,dateAndTime = filename.split("-")[0],filename.split("-")[1],filename.split("-")[2]
+     cam = dr.Camera()
+     model = dr.Model()
+     takePictureForEvery = 5
+     totalDuration = 1*60
+     totalFrames = totalDuration//takePictureForEvery
+     for i in range(totalFrames):
+          frame = cam.getFrame()
+          resized_frame = cam.resizeFrame(frame,(48,48))
+          preprocessed_frame = model.preprocess(resized_frame)
+          predictions = model.predict(preprocessed_frame)
+          predictions['class_Id'] = [classId]
+          predictions['facultyId'] = [facultyId]
+          curr = str(datetime.today()).split()
+          predictions['timeStamp'] = [curr[1][:8]]
+          dfe = pd.DataFrame(predictions)
+          if i==0:
+               dfe.to_csv(os.path.join(settings.BASE_DIR,'static','resources',filename),index=False)
+          else:
+               dfe.to_csv(os.path.join(settings.BASE_DIR,'static','resources',filename),index=False,mode='a',header=False)
+
+          print(f"Analytics {i} Recorded")
+          time.sleep(takePictureForEvery)
+     return HttpResponse("Sucess")
+          
+          
 
 
 
